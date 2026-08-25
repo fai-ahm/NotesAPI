7171dba9eb52af7a93e6c4a7d20ea10b7e989a7f
Faisal Ahmad <sixfaisal@gmail.com>
2026-08-25 19:22:40 +0200

    Add .gitignore file and update AppDbContext structure

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,2 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Learning1.Models;
+
+namespace Learning1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
--- obj/Debug/net10.0/Learning1.AssemblyInfo.cs
+++ obj/Debug/net10.0/Learning1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d322b84769ca53789556eeeb44ea4dccc636e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
